936dcc3da81e54a0fb301b0651fe3055b9e44296
HuaLian <zawkhantwin132004@gmail.com>
2024-01-24 23:41:43 +0630

    Commit

--- Admin/includes/sidebar.php
+++ Admin/includes/sidebar.php
@@ -1,12 +1,11 @@
 <?php 
-  
+  session_start();
   $page=substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'],"/")+1);
 ?>
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
-       
+      <a class="navbar-brand m-0"  target="_blank">
         <span class="ms-1 font-weight-bold text-white">Electricity Billing System</span>
       </a>
     </div>
@@ -22,6 +21,14 @@
           </a>
         </li>
         <li class="nav-item">
+          <a class="nav-link text-white <?= $page == "customer.php" ? 'active bg-gradient-primary':''?>" href="customer.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">people</i>
+            </div>
+            <span class="nav-link-text ms-1">Customer</span>
+          </a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link text-white <?= $page == "bill.php" ? 'active bg-gradient-primary':''?>" href="bill.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">table_view</i>
@@ -32,14 +39,21 @@
         <li class="nav-item">
           <a class="nav-link text-white " href="../pages/dashboard.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">dashboard</i>
+              <i class="material-icons opacity-10">warning</i>
             </div>
-            <span class="nav-link-text ms-1">Dashboard</span>
+            <span class="nav-link-text ms-1">Complain</span>
           </a>
         </li>
+        
       </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
+    <div class="mx-3">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">person</i>
+              <span class="nav-link-text ms-1"><?php echo $_SESSION['admin'] ?></span>
+            </div>
+    </div>
       <div class="mx-3">
         <a class="btn bg-gradient-primary mt-4 w-100" href="sub/logout.php" type="button">Logout</a>
       </div>
